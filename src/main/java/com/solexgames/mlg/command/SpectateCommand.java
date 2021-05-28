d8572b754e0c11642fa981d6554f7df96914e08a
GrowlyX <rtaofficialtechie@Gmail.com>
2021-05-28 18:41:14 -0400

    Added a /spectate <target> command to spectate a specific player without having to go through the menu

--- src/main/java/com/solexgames/mlg/command/SpectateCommand.java
+++ src/main/java/com/solexgames/mlg/command/SpectateCommand.java
@@ -2,7 +2,13 @@ package com.solexgames.mlg.command;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
+import co.aikar.commands.bukkit.contexts.OnlinePlayer;
+import com.solexgames.mlg.CorePlugin;
 import com.solexgames.mlg.menu.impl.MatchSpectateMenu;
+import com.solexgames.mlg.model.Arena;
+import com.solexgames.mlg.state.impl.ArenaState;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 /**
@@ -10,10 +16,27 @@ import org.bukkit.entity.Player;
  * @since 5/28/2021
  */
 
+@CommandAlias("spectate|spec")
 public class SpectateCommand extends BaseCommand {
 
-    @CommandAlias("spectate|spec")
-    public void execute(Player player) {
+    @Default
+    public void onDefault(Player player) {
         new MatchSpectateMenu().openMenu(player);
+    }
+
+    public void execute(Player player, OnlinePlayer target) {
+        final Arena arena = CorePlugin.getInstance().getArenaHandler().getByPlayer(target.getPlayer());
+
+        if (arena == null) {
+            player.sendMessage(ChatColor.RED + "Error: That player is not in a match.");
+            return;
+        }
+
+        if (!arena.getState().equals(ArenaState.IN_GAME)) {
+            player.sendMessage(ChatColor.RED + "Error: That match has not started yet or the arena binded to it is regenerating.");
+            return;
+        }
+
+        CorePlugin.getInstance().getArenaHandler().startSpectating(player, arena);
     }
 }
