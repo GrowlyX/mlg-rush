52bf64bfd980e746f7dbc0d9a20b6b738f47d7fb
GrowlyX <rtaofficialtechie@Gmail.com>
2021-05-28 15:47:49 -0400

    Added a constant author string array to easily update the list of authors

--- src/main/java/com/solexgames/mlg/command/MLGRushCommand.java
+++ src/main/java/com/solexgames/mlg/command/MLGRushCommand.java
@@ -5,10 +5,18 @@ import co.aikar.commands.annotation.CommandAlias;
 import com.solexgames.mlg.util.Color;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MLGRushCommand extends BaseCommand {
+
+    private final static String[] AUTHORS = new String[]{
+            "GrowlyX#4953",
+            "puugz#4877",
+    };
 
     @CommandAlias("mlgrush")
     public void mlgRush(Player player) {
-        player.sendMessage(Color.SECONDARY + "This server is running " + Color.PRIMARY + "MLGRush" + Color.SECONDARY + " created by " + Color.PRIMARY + "GrowlyX#4953 & puugz#4877" + Color.SECONDARY + "!");
+        player.sendMessage(Color.SECONDARY + "This server is running " + Color.PRIMARY + "MLGRush" + Color.SECONDARY + " created by " + Color.PRIMARY + String.join(Color.SECONDARY + ", " + Color.PRIMARY, AUTHORS) + Color.SECONDARY + ".");
     }
 }
