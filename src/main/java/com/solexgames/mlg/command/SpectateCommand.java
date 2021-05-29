bf16f56d228a6dd94bcc5b361d904e877a2458b6
puugz <matej1.svetina@gmail.com>
2021-05-29 10:34:27 +0200

    Fixed being able to spectate while in a game

--- src/main/java/com/solexgames/mlg/command/SpectateCommand.java
+++ src/main/java/com/solexgames/mlg/command/SpectateCommand.java
@@ -3,6 +3,7 @@ package com.solexgames.mlg.command;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Optional;
 import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import com.solexgames.mlg.CorePlugin;
 import com.solexgames.mlg.menu.impl.MatchSpectateMenu;
@@ -16,22 +17,27 @@ import org.bukkit.entity.Player;
  * @since 5/28/2021
  */
 
-@CommandAlias("spectate|spec")
 public class SpectateCommand extends BaseCommand {
 
-    @Default
-    public void onDefault(Player player) {
-        new MatchSpectateMenu().openMenu(player);
-    }
+    @CommandAlias("spectate|spec")
+    public void execute(Player player, @Optional OnlinePlayer target) {
+        final Arena arena = CorePlugin.getInstance().getArenaHandler().getByPlayer(player);
 
-    public void execute(Player player, OnlinePlayer target) {
-        final Arena arena = CorePlugin.getInstance().getArenaHandler().getByPlayer(target.getPlayer());
-        final Arena targetArena = CorePlugin.getInstance().getArenaHandler().getByPlayer(target.getPlayer());
+        if (target == null) {
+            if (arena != null) {
+                player.sendMessage(ChatColor.RED + "Error: You are already in an arena.");
+                return;
+            }
+            new MatchSpectateMenu().openMenu(player);
+            return;
+        }
 
         if (arena != null) {
             player.sendMessage(ChatColor.RED + "Error: You are already in an arena.");
             return;
         }
+
+        final Arena targetArena = CorePlugin.getInstance().getArenaHandler().getByPlayer(target.getPlayer());
 
         if (targetArena == null) {
             player.sendMessage(ChatColor.RED + "Error: That player is not in a match.");
